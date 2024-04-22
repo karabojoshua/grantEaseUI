44744d2279ed7533f5559f5a47944c763a2386e6
karabojoshua <2580661@students.wits.ac.za>
2024-04-22 10:54:27 +0000

    Small Changes Before Deployment

---
+++ fb.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAi6nukaw0FvIML1yI7cH82uuShd57oH1A",
+  authDomain: "grantease-a004a.firebaseapp.com",
+  projectId: "grantease-a004a",
+  storageBucket: "grantease-a004a.appspot.com",
+  messagingSenderId: "812079999120",
+  appId: "1:812079999120:web:4c680e4e4536fb37135015",
+  measurementId: "G-JSKJBMX177"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
